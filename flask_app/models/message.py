839b74db9d9f67521143999391fd343a44e9e7c1
Richard Chu <rchu1211@gmail.com>
2022-04-18 22:21:34 -0700

    initial commit

--- flask_app/models/message.py
+++ flask_app/models/message.py
@@ -1,5 +1,7 @@
 from flask import flash
 from flask_app.config.mysqlconnection import connectToMySQL
+from datetime import datetime
+import math
 
 
 class Message:
@@ -7,24 +9,50 @@ class Message:
 
     def __init__(self, data):
         self.id = data['id']
-        self.message = data['message']
+        self.content = data['content']
+        self.sender_id = data['sender_id']
+        self.sender = data['sender']
+        self.receiver = data['receiver_id']
+        self.receiver = data['receiver']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
+    def time_span(self):
+        now = datetime.now()
+        delta = now - self.created_at
+        print(delta.days)
+        print(delta.total_seconds())
+        if delta.days > 0:
+            return f"{delta.days} days ago"
+        elif (math.floor(delta.total_seconds() / 60)) >= 60:
+            return f"{math.floor(math.floor(delta.total_seconds() / 60)/60)} hours ago"
+        elif delta.total_seconds() >= 60:
+            return f"{math.floor(delta.total_seconds() / 60)} minutes ago"
+        else:
+            return f"{math.floor(delta.total_seconds())} seconds ago"
+
     @classmethod
-    def get_all(cls):
-        query = "SELECT * FROM messages;"
-        results = connectToMySQL(cls.db).query_db(query)
+    def get_user_messages(cls, data):
+        query = "SELECT users.first_name as sender, users2.first_name as receiver, messages.* FROM users LEFT JOIN messages ON users.id = messages.sender_id LEFT JOIN users as users2 ON users2.id = messages.receiver_id WHERE users2.id =  %(id)s"
+        results = connectToMySQL(cls.db_name).query_db(query, data)
         messages = []
-        for row in results:
-            messages.append(cls(row))
+        for message in results:
+            messages.append(cls(message))
+        return messages
+
+    @classmethod
+    def get_user_messages(cls, data):
+        query = "SELECT users.first_name as sender, users2.first_name as receiver, messages.* FROM users LEFT JOIN messages ON users.id = messages.sender_id LEFT JOIN users as users2 ON users2.id = messages.receiver_id WHERE users2.id =  %(id)s"
+        results = connectToMySQL(cls.db_name).query_db(query, data)
+        messages = []
+        for message in results:
+            messages.append(cls(message))
         return messages
 
     @classmethod
     def save(cls, data):
-        query = """INSERT INTO messages (message)
-        VALUES (%(messages)s;"""
-        return connectToMySQL(cls.db).query_db(query, data)
+        query = "INSERT INTO messages (content,sender_id,receiver_id) VALUES (%(content)s,%(sender_id)s,%(receiver_id)s);"
+        return connectToMySQL(cls.db_name).query_db(query, data)
 
     @classmethod
     def get_from_id(cls, data):
@@ -37,6 +65,11 @@ class Message:
         if len(results) < 1:
             return False
         return cls(results[0])
+
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM messages WHERE messages.id = %(id)s;"
+        return connectToMySQL(cls.db_name).query_db(query, data)
 
     @staticmethod
     def validate_message(message):
